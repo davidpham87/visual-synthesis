4a02959b87d143771ea08998d277fe7dbecb4ea5
David Pham <davidpham87@gmail.com>
2024-04-24 00:15:03 +0200

    feat: update abstract

--- src/cljs/wyssacademy/visual_synthesis/about.cljs
+++ src/cljs/wyssacademy/visual_synthesis/about.cljs
@@ -5,36 +5,40 @@
 (defn about []
   [:div.w-full.justify-center.grid.px-10.pb-10.transition-opacity.opacity-100
    [:div {:class ["text-justify" "max-w-xl"]}
-    [typography/paragraph "Many social-ecological systems are in an
-unsustainable state. Bringing together disjunct published findings on complex
-interactions in social-ecological systems may enable the identification of
-leverage points for transformations towards sustainability. However, such
-interdisciplinary synthesis studies on specific regional social-ecological
-systems main rare."]
     [typography/paragraph
-     "Here, we pair a review of systematically identified studies
-with a cross-impact analysis to create an interactive visual social-ecological
-systems synthesis on nflicts and synergies between land use, biodiversity
-conservation, and human wellbeing in north-eastern Madagascar. "]
+     "Many social-ecological systems are in an unsustainable state. Bringing
+     together disjunct published findings on complex interactions in
+     social-ecological systems may enable the identification of leverage points
+     for transformations towards sustainability. However, such
+     interdisciplinary synthesis studies on specific regional social-ecological
+     systems remain rare."]
+    [typography/paragraph
+     "Here, we pair a review of systematically identified studies with a
+     cross-impact analysis to create an interactive visual social-ecological
+     systems synthesis on conflicts and synergies between land use,
+     biodiversity conservation, and human wellbeing in north-eastern
+     Madagascar."]
     [typography/paragraph
      [:a {:href "https://visualsynthesis.wyssacademy.org"
           :style {:color "blue"}}
       "The interactive visual synthesis"]
      " depicts an archetypical regional landscape with 22 factors comprising
-physical landscape elements, ecosystem services, wellbeing, human activities,
-and telecouplings. To understand the connections between these factors, we
-assess directional causal links based on literature sources. The visual
-synthesis shows that research has so far focused on links between land use and
-biodiversity while links to human wellbeing were studied more seldomly. We then
-identify chains and cycles that emerge from the links between factors and rate
-them based on their plausibility and relevance. "]
-    [typography/paragraph "All eight top-rated chains and cycles relate to subsistence and
-commercial agriculture, revealing promising leverage points at which
-interventions could improve outcomes for biodiversity and wellbeing. In sum, we
-show how interactive visual syntheses can be a useful way to make disjunct
-published findings on regional social-ecological systems more accessible, to
-find research gaps, and to identify leverage points for sustainability
-transformations."]
+     physical landscape elements, ecosystem services, wellbeing, human
+     activities, and telecouplings. To understand the connections between these
+     factors, we assess directional causal links based on literature
+     sources. The visual synthesis shows that research has so far focused on
+     links between land use and biodiversity while links to human wellbeing
+     were studied more seldomly. We then identify chains and cycles that emerge
+     from the links between factors and rate them based on their plausibility
+     and relevance."]
+    [typography/paragraph
+     "All eight top-rated chains and cycles relate to subsistence and
+     commercial agriculture, revealing promising leverage points at which
+     interventions could improve outcomes for biodiversity and wellbeing. In
+     sum, we show how interactive visual syntheses can be a useful way to make
+     disjunct published findings on regional social-ecological systems more
+     accessible, to find research gaps, and to identify leverage points for
+     sustainability transformations."]
     [:div.py-3]
     [typography/paragraph
      [:b "Please cite this work as: "]
